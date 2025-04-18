5b3bb6239292696b5e3543a22e15a73d76b7bdca
stewartong <ong.hueh.chuah@gmail.com>
2025-04-19 05:40:05 +0800

    19Apr 6th commit Adding new s3 bucket

--- main.tf
+++ main.tf
@@ -1,10 +1,21 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "stewart-tf-test-bucket1"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce9-tfstate"
+    key    = "stewart-s3-tf-ci.tfstate"  #Change this
+    region = "us-east-1"
   }
 }
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  name_prefix = "${split("/", "${data.aws_caller_identity.current.arn}")[1]}"
+  account_id  = "${data.aws_caller_identity.current.account_id}"
+}
+
+resource "aws_s3_bucket" "s3_tf" {
+  bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
+}
+
 
 
